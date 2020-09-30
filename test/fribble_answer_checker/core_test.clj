d7dc2307d8f13e2e97ed06b423b45afd568b10ae
OJIMA Ryoji <rojima1@gmail.com>
2020-09-30 14:51:13 +0900

    readme is deleted.

--- test/fribble_answer_checker/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns fribble-answer-checker.core-test
-  (:require [clojure.test :refer :all]
-            [fribble-answer-checker.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
